88e1f0cf49b165390f3675fa46efecb0a27ad8b3
Gabriel <gabrielvasconcellos76@gmail.com>
2022-12-13 16:30:41 -0300

    RPG-js-concluido

--- Lógica/7-CalculoDano/index.js
+++ Lógica/7-CalculoDano/index.js
@@ -1,26 +1,45 @@
-const nomePersonagem1 = prompt("Digite o nome do seu personagem de ataque:")
-const poder = Number(prompt("Digite o poder de ataque dele:"))
+const nomePersonagem1 = prompt("Digite o nome do seu personagem de ataque:");
+const poder = Number(prompt("Digite o poder de ataque dele:"));
 
-const nomePersonagem2 = prompt("Digite o nome do seu personagem de defesa:")
-const vidaInicial = Number(prompt("Digite a quantidade de vida dele:"))
-const defesa = Number(prompt("Digite o poder de defesa dele:"))
+const nomePersonagem2 = prompt("Digite o nome do seu personagem de defesa:");
+const vidaInicial = Number(prompt("Digite a quantidade de vida dele:"));
+const defesa = Number(prompt("Digite o poder de defesa dele:"));
+const escudo = prompt("Sua defesa possui um escudo S / N:").toUpperCase();
 
-const escudo = prompt("Sua defesa possui um escudo S / N:").toUpperCase
+let dano = 0;
+let vidaAtual = 0;
 
-let dano = 0
-let vidaAtual = 0
-
-if(poder > defesa && escudo === "N"){
-    dano += poder - defesa
-    console.log(dano)
-    vidaAtual += vidaInicial - dano
-    alert(`Os jogadores possuem os seguintes atributos:\n\nAtaque\nNome: ${nomePersonagem1}\nPoder de ataque: ${poder}\n\nDefensor\nNome: ${nomePersonagem2}\nDano Sofrido: ${dano}\nVida Atual: ${vidaAtual}`)
-}
-else if(poder > defesa && escudo === "S"){
-    dano += (poder - defesa) / 2 
-    console.log(dano)
-}
-else if(poder <= defesa){
-    dano = 0
-    console.log("o dano é igual a zero")
+if (poder > defesa && escudo === "N") {
+  dano = poder - defesa;
+  vidaAtual += vidaInicial - dano;
+  alert(
+    `Os jogadores possuem os seguintes atributos:\n\nAtaque\nNome: ${nomePersonagem1}\nPoder de ataque: ${poder}\n\nDefensor\nNome: ${nomePersonagem2}\nDano Sofrido: ${dano}\nVida Atual: ${vidaAtual}`
+  );
+} else if (poder > defesa && escudo === "S") {
+  dano += (poder - defesa) / 2;
+  vidaAtual += vidaInicial - dano;
+  alert(
+    `Os jogadores possuem os seguintes atributos:
+    \n\nAtaque
+    \nNome: ${nomePersonagem1}
+    \nPoder de ataque: ${poder}
+    \n\nDefensor
+    \nNome: ${nomePersonagem2}
+    \nDano Sofrido: ${dano}
+    \nVida Atual: ${vidaAtual}`
+  );
+} else if (poder <= defesa) {
+  dano = 0;
+  vidaAtual += vidaInicial - dano;
+  console.log("o dano é igual a zero");
+  alert(
+    `Os jogadores possuem os seguintes atributos:
+    \n\nAtaque
+    \nNome: ${nomePersonagem1}
+    \nPoder de ataque: ${poder}
+    \n\nDefensor
+    \nNome: ${nomePersonagem2}
+    \nDano Sofrido: ${dano}
+    \nVida Atual: ${vidaAtual}`
+  );
 }
