406dec82aa1ddc25b9224da49e64778118802bf7
Gabriel <gabrielvasconcellos76@gmail.com>
2022-12-15 16:09:25 -0300

    update-script-ExFinanças-switch

--- Lógica/14-exFinanceiro/script.js
+++ Lógica/14-exFinanceiro/script.js
@@ -1,3 +1,29 @@
+// let qtdeInicial = 0,
+//   opcoes,
+//   adicionar,
+//   remover;
+
+// qtdeInicial = Number(prompt("Digite a quantidade inicial de dinheiro:"));
+// do {
+//   opcoes = prompt(
+//     `Você possui ${qtdeInicial} deseja
+//\n1) adicionar dinheiro
+//\n2) remover dinheiro
+//\n3) encerrar o programa`
+//   );
+
+//   if (opcoes === "1") {
+//     adicionar = Number(prompt("Quanto deseja adicionar?"));
+//     qtdeInicial += adicionar;
+//   } else if (opcoes === "2") {
+//     remover = Number(prompt("Quanto deseja remover?"));
+//     qtdeInicial -= remover;
+//   }
+// } while (opcoes !== "3");
+// alert("Você encerrou o programa");
+
+//versão com switch / case
+
 let qtdeInicial = 0,
   opcoes,
   adicionar,
@@ -6,15 +32,26 @@ let qtdeInicial = 0,
 qtdeInicial = Number(prompt("Digite a quantidade inicial de dinheiro:"));
 do {
   opcoes = prompt(
-    `Você possui ${qtdeInicial} deseja\n1) adicionar dinheiro\n2) remover dinheiro\n3) encerrar o programa`
+    `Você possui ${qtdeInicial}R$ o que deseja?
+    \n1) adicionar dinheiro
+    \n2) remover dinheiro
+    \n3) encerrar o programa`
   );
 
-  if (opcoes === "1") {
-    adicionar = Number(prompt("Quanto deseja adicionar?"));
-    qtdeInicial += adicionar;
-  } else if (opcoes === "2") {
-    remover = Number(prompt("Quanto deseja remover?"));
-    qtdeInicial -= remover;
+  switch(opcoes){
+    case "1":
+      adicionar = Number(prompt("Quanto deseja adicionar?"));
+      qtdeInicial += adicionar;
+      break
+    case "2":
+      remover = Number(prompt("Quanto deseja remover?"));
+      qtdeInicial -= remover;
+      break
+    case "3":
+      alert("saindo...")
+      break
+    default:
+      alert("Opção Invalida!")
   }
 } while (opcoes !== "3");
 alert("Você encerrou o programa");
