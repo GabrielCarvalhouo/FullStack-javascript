a4237f61c48a49711fe91bc3f6cbc5d52dfcae97
Gabriel <gabrielvasconcellos76@gmail.com>
2022-12-15 17:51:48 -0300

    aula15-sobre-For

---
+++ Lógica/15-for/index.js
@@ -0,0 +1,12 @@
+//inicialização; condição; finalização
+
+let i = 0
+
+for(
+    alert("inicializando o for...");
+    i<=10;
+    alert("Finalizando um bloco de repetição..")
+){
+    alert(i)
+    i++
+}
