0c4648c25d8c6a956b42c8713454d7cdec13ca18
Gabriel <gabrielvasconcellos76@gmail.com>
2022-12-15 15:53:36 -0300

    update-exMenu

--- Lógica/13-exmenuInterativo/index.js
+++ Lógica/13-exmenuInterativo/index.js
@@ -4,6 +4,26 @@ do{
 
     opcoes = prompt("Escolha umas das opcoes\n\n1)\n2)\n3)\n4)\n5): Encerrar")
 
+    switch(opcoes){
+        case "1":
+            alert("opção 1")
+        break
+        case "2":
+            alert("opção 2")
+        break
+        case "3":
+            alert("opção 3")
+        break
+        case "4": 
+            alert("opção 4")
+        break
+        case "5":
+            alert("Você escolheu encerrar")
+            alert("Encerrando....")
+        break
+        default:
+            alert("opção Invalida")
+    }
 }while(opcoes !== '5')
 
 alert("Você saiu do programa")
