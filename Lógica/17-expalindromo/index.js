16292611961ebebdd6af078cc9376e34873b6377
Gabriel <gabrielvasconcellos76@gmail.com>
2022-12-16 15:24:29 -0300

    exercicio palindromo finalizado

---
+++ Lógica/17-expalindromo/index.js
@@ -0,0 +1,14 @@
+const palavra = prompt("Digite uma palavra:")
+
+let palavraInvertida = ""
+
+for (i = palavra.length - 1; i >=0; i--){
+    palavraInvertida += palavra[i]
+}
+
+if(palavra === palavraInvertida){
+    alert(palavra + " é um palindromo!")
+}else{
+    alert(palavra + " Não é um palindromo!\n\n"
+    + palavra + " !== " + palavraInvertida)
+}
